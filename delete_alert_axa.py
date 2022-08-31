1433a03482ca2a3309048375b1f2c3dba15fdf07
Doğaç Topay <44817453+dogactopay@users.noreply.github.com>
2022-08-31 11:17:50 +0300

    Update delete_alert_axa.py

--- delete_alert_axa.py
+++ delete_alert_axa.py
@@ -6,5 +6,20 @@ app = FastAPI()
 
 @app.get("/delete_alert/{alert_id}")
 async def read_item(alert_id):
-    resp = requests.delete(f"https://axa-alerts.monitorpark.net/api/alert/{alert_id}",headers={'Authorization': f'Key PRhOXD4TGnO2JTxKB2kRZRhDbHT7caIAqErn32A7','content-type': 'application/json'}).text
-    return resp
+    resp = requests.put(f"https://axa-alerts.monitorpark.net/api/alert/{alert_id}",headers={'Authorization': f'Key PRhOXD4TGnO2JTxKB2kRZRhDbHT7caIAqErn32A7','content-type': 'application/json'}).text
+    return 
+
+
+
+
+
+
+
+class alert_close(BaseModel):
+    alert_id: str
+    
+
+@app.post("/delete_alert_post/")
+async def signup(item: alert_close):
+    resp = requests.put(f"https://axa-alerts.monitorpark.net/api/alert/{dict(item)['alert_id']}",headers={'Authorization': f'Key PRhOXD4TGnO2JTxKB2kRZRhDbHT7caIAqErn32A7','content-type': 'application/json'}).text
+    return status
